1702051ef0872f4996853d8fbe270bc53476882b
JimmyCodeMunity <jameswafula2002@gmail.com>
2024-11-17 00:59:55 +0300

    udated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="h-screen w-full flex flex-row justify-center items-center bg-black">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1 className="text-white">Texting</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
